22a8ba890063283308ebf562be814001edd11a99
jayashz <jayzstha15@gmail.com>
2024-10-20 14:40:50 +0545

    Added:Project Setup

---
+++ src/app/(tabs)/_layout.tsx
@@ -0,0 +1,53 @@
+import { Tabs } from "expo-router";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+export default function TabsLayout() {
+  return (
+    <Tabs
+      screenOptions={{ tabBarActiveTintColor: "black", tabBarShowLabel: false }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          headerTitle: "For you",
+          tabBarIcon: ({ color }) => (
+            <MaterialIcons name="home-filled" size={26} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="search"
+        options={{
+          tabBarIcon: ({ color }) => (
+            <FontAwesome name="search" size={26} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="post"
+        options={{
+          tabBarIcon: ({ color }) => (
+            <FontAwesome name="plus-square-o" size={26} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="reels"
+        options={{
+          tabBarIcon: ({ color }) => (
+            <MaterialIcons name="smart-display" size={29} color="black" />
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name="profile"
+        options={{
+          tabBarIcon: ({ color }) => (
+            <FontAwesome name="user-circle-o" size={26} color="black" />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
